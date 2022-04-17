10d5acc8ee0f526e1863dc790acf7adbd0efc0a1
Miguel Figuera <miguelqui725@gmail.com>
2022-04-17 00:43:59 -0400

    working on transforming the answer into the new a

--- script.js
+++ script.js
@@ -49,7 +49,10 @@ function eraser(){
     .toString()
     .slice(0,-1);
 }
-
+function screenReseter() {
+    if (screenInput.textContent == "0") {screenInput.textContent = "";}
+    else return;
+}
 function addNum(num){
     screenInput.textContent += num;
 }
@@ -57,22 +60,52 @@ function addNum(num){
 function operator(operator){
     screenInput.textContent += operator;
 }
+function evaluate(){
+    operation = screenInput.textContent
+ if (operation.indexOf("+") > -1 ) {
+     array = operation.split("+");
+     a =parseInt(array[0],10);
+     b =parseInt(array[1],10);
+     screenAnswer.textContent= add(a, b);} 
+if (operation.indexOf("-") > -1) {     
+    array = operation.split("-");
+    a =parseInt(array[0],10);
+    b =parseInt(array[1],10);
+    screenAnswer.textContent= minus(a, b);  }
+if (operation.indexOf("*") > -1) {     
+    array = operation.split("*");
+    a =parseInt(array[0],10);
+    b =parseInt(array[1],10);
+    screenAnswer.textContent= multiply(a, b);  }
+if (operation.indexOf("/") > -1){
+    array = operation.split("/");
+    a =parseInt(array[0],10);
+    b =parseInt(array[1],10);
+    screenAnswer.textContent= divide(a, b);  }
+if (operation.indexOf("%") > -1){
+    array = operation.split("%");
+    a =parseInt(array[0],10);
+    b =parseInt(array[1],10);
+    screenAnswer.textContent= percent(a, b);  }
+}
 
 eraserBtn.onclick = () => {eraser()};
 allClearBtn.onclick= () => {clearAll()}
+equalBtn.onclick= () => {evaluate()}
+
 operatorBtns[0].onclick= () =>{operator(operatorBtns[0].textContent)}
 operatorBtns[1].onclick= () =>{operator(operatorBtns[1].textContent)}
 operatorBtns[2].onclick= () =>{operator(operatorBtns[2].textContent)}
 operatorBtns[3].onclick= () =>{operator(operatorBtns[3].textContent)}
 operatorBtns[4].onclick= () =>{operator(operatorBtns[4].textContent)}
 
-numberBtns[0].onclick= () =>{addNum(numberBtns[0].textContent)}
-numberBtns[1].onclick= () =>{addNum(numberBtns[1].textContent)}
-numberBtns[2].onclick= () =>{addNum(numberBtns[2].textContent)}
-numberBtns[3].onclick= () =>{addNum(numberBtns[3].textContent)}
-numberBtns[4].onclick= () =>{addNum(numberBtns[4].textContent)}
-numberBtns[5].onclick= () =>{addNum(numberBtns[5].textContent)}
-numberBtns[6].onclick= () =>{addNum(numberBtns[6].textContent)}
-numberBtns[7].onclick= () =>{addNum(numberBtns[7].textContent)}
-numberBtns[8].onclick= () =>{addNum(numberBtns[8].textContent)}
-numberBtns[9].onclick= () =>{addNum(numberBtns[9].textContent)}
+numberBtns[0].onclick= () =>{screenReseter(); addNum(numberBtns[0].textContent)}
+numberBtns[1].onclick= () =>{screenReseter(); addNum(numberBtns[1].textContent)}
+numberBtns[2].onclick= () =>{screenReseter(); addNum(numberBtns[2].textContent)}
+numberBtns[3].onclick= () =>{screenReseter(); addNum(numberBtns[3].textContent)}
+numberBtns[4].onclick= () =>{screenReseter(); addNum(numberBtns[4].textContent)}
+numberBtns[5].onclick= () =>{screenReseter(); addNum(numberBtns[5].textContent)}
+numberBtns[6].onclick= () =>{screenReseter(); addNum(numberBtns[6].textContent)}
+numberBtns[7].onclick= () =>{screenReseter(); addNum(numberBtns[7].textContent)}
+numberBtns[8].onclick= () =>{screenReseter(); addNum(numberBtns[8].textContent)}
+numberBtns[9].onclick= () =>{screenReseter(); addNum(numberBtns[9].textContent)}
