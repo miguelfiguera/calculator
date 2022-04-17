5879cf664740b1ba6f33772584bc99564d689a5b
Miguel Figuera <miguelqui725@gmail.com>
2022-04-17 00:03:33 -0400

    input with digital pad done, working on this making the basic operations

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function eraser(){
 }
 
 function addNum(num){
-    screenInput.textContent += number;
+    screenInput.textContent += num;
 }
 
 function operator(operator){
@@ -60,5 +60,19 @@ function operator(operator){
 
 eraserBtn.onclick = () => {eraser()};
 allClearBtn.onclick= () => {clearAll()}
+operatorBtns[0].onclick= () =>{operator(operatorBtns[0].textContent)}
+operatorBtns[1].onclick= () =>{operator(operatorBtns[1].textContent)}
+operatorBtns[2].onclick= () =>{operator(operatorBtns[2].textContent)}
+operatorBtns[3].onclick= () =>{operator(operatorBtns[3].textContent)}
+operatorBtns[4].onclick= () =>{operator(operatorBtns[4].textContent)}
 
-    
+numberBtns[0].onclick= () =>{addNum(numberBtns[0].textContent)}
+numberBtns[1].onclick= () =>{addNum(numberBtns[1].textContent)}
+numberBtns[2].onclick= () =>{addNum(numberBtns[2].textContent)}
+numberBtns[3].onclick= () =>{addNum(numberBtns[3].textContent)}
+numberBtns[4].onclick= () =>{addNum(numberBtns[4].textContent)}
+numberBtns[5].onclick= () =>{addNum(numberBtns[5].textContent)}
+numberBtns[6].onclick= () =>{addNum(numberBtns[6].textContent)}
+numberBtns[7].onclick= () =>{addNum(numberBtns[7].textContent)}
+numberBtns[8].onclick= () =>{addNum(numberBtns[8].textContent)}
+numberBtns[9].onclick= () =>{addNum(numberBtns[9].textContent)}
