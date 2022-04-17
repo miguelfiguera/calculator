dedf361c8dbeab7bf3e88cac4b7b2d131e78d2ca
Miguel Figuera <miguelqui725@gmail.com>
2022-04-16 23:40:59 -0400

    funciones de botones ver 1

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNum = "";
+let secondNum= "";
+let currentOperator = null;
+
 //Buttons
 const allClearBtn = document.getElementById("btnClear");
 const eraserBtn = document.getElementById("btnEraser");
@@ -6,6 +10,9 @@ const equalBtn=document.getElementById("btnEqual");
 const operatorBtns=document.getElementsByClassName("operator");
 const numberBtns=document.getElementsByClassName("number");
 
+// Screen elements
+const screenInput=document.getElementById("input");
+const screenAnswer = document.getElementById("answer");
 
 // Basic operations
 function add(a,b){
@@ -24,3 +31,34 @@ function divide(a,b) {
 function percent(a,b){
     return a*(b/100)
 }
+
+//Keyboard Input
+
+
+//The button Functions
+function clearAll(){
+    screenInput.textContent = "0";
+    screenAnswer.textContent= "";
+    firstNum = "";
+    secondNum= "";
+    currentOperator = "";
+}
+
+function eraser(){
+    screenInput.textcontent = screenInput.textContent
+    .toString()
+    .slice(0,-1);
+}
+
+function addNum(num){
+    screenInput.textContent += number;
+}
+
+function operator(operator){
+    screenInput.textContent += operator;
+}
+
+eraserBtn.onclick = () => {eraser()};
+allClearBtn.onclick= () => {clearAll()}
+
+    
