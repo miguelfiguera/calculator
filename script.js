84d41fc934a0baa2a057a8c8e3d8cb0070e5fc21
Miguel Figuera <miguelqui725@gmail.com>
2022-04-16 22:43:38 -0400

    version 1

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//Buttons
+const allClearBtn = document.getElementById("btnClear");
+const eraserBtn = document.getElementById("btnEraser");
+const decimalBtn = document.getElementById("btnDecimal");
+const equalBtn=document.getElementById("btnEqual");
+const operatorBtns=document.getElementsByClassName("operator");
+const numberBtns=document.getElementsByClassName("number");
+
+
 // Basic operations
 function add(a,b){
     return a+b
@@ -9,7 +18,7 @@ function multiply(a,b){
     return a*b
 }
 function divide(a,b) {
-    if(b === 0) {return "ERROR! Only Chuck Norris can divide by zero!"}
+    if(b === 0) {return alert("ERROR! Only Chuck Norris can divide by zero!")}
     return a/b
 }
 function percent(a,b){
